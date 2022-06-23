1bbdbbe7128a105fa0be00376cc25f7f49bf99fd
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2022-06-23 14:39:06 +0300

    add swagger

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+
 	"books-api/handler"
 	"books-api/repository"
 	"books-api/server"
@@ -9,8 +10,19 @@ import (
 	"github.com/spf13/viper"
 	"github.com/subosito/gotenv"
 	"os"
-)
 
+
+)
+// @title Books API
+// @version 1.0
+// @description API Server for Books-api Application
+
+// @host localhost:8000
+// @BasePath /
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main(){
 
 	if err := initConfig(); err != nil{
