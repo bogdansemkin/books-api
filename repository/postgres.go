1924337191defd328a77132ed08e48859cb200f3
Bogdan Donchenko <40574816+goddamityouscrewedupagain@users.noreply.github.com>
2022-06-23 13:52:37 +0300

    add logrus

--- repository/postgres.go
+++ repository/postgres.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"log"
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -25,12 +25,12 @@ func NewPostgresDB(cfg Config) (*sqlx.DB, error){
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBname, cfg.Password, cfg.SSLMode))
 	if err != nil{
-		log.Fatalf("Error during connection to database : %s", err)
+		logrus.Errorf("Error during connection to database : %s", err)
 		return nil, err
 	}
 	err = db.Ping()
 	if err != nil{
-		log.Fatalf("Error during binding to database : %s", err)
+		logrus.Errorf("Error during binding to database : %s", err)
 		return nil, err
 	}
 	return db, nil
